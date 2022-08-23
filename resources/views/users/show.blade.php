9309febc9787f5d69f0b57afbfab1db889b82f39
Ratchathorn <ratchathorn.s@ku.th>
2022-08-23 09:54:18 +0700

    you do not need to login to see user profiles

--- resources/views/users/show.blade.php
+++ resources/views/users/show.blade.php
@@ -1,9 +1,10 @@
 @extends('layouts.main')
 
 @section('content')
+{{--    @can('create', Post::class)--}}
     <section class="mx-8">
         <div class="overflow-hidden relative w-40 h-40 bg-gray-800 rounded-full">
-        <img src="{{ url('storage/profiles/'.Auth::user()->profile_image) }}" alt="" title=""/>
+        <img src="{{ url('storage/profiles/'.$user->profile_image) }}" alt="" title=""/>
         </div>
         <br><h1 class="text-3xl mt-6">
             {{ $user->name }}
@@ -23,7 +24,11 @@
         <br class="mx-8 mt-2">
             Description: {{$user->description}}
         <br>
-        <a class="mt-4 flex w-14 rounded-lg bg-gray-800 hover:bg-gray-600 focus:ring-gray-700 text-white border-gray-600" href="{{route('users.edit', ['user'=>Auth::user()->id])}}"><div class="ml-3 my-2">edit</div></a>
+        @auth
+        @if (Auth::user()->id == $user->id)
+        <a class="mt-4 flex w-14 rounded-lg bg-gray-800 hover:bg-gray-600 focus:ring-gray-700 text-white border-gray-600" href="{{route('users.edit', ['user'=>$user->id])}}"><div class="ml-3 my-2">edit</div></a>
+            @endif
+            @endauth
     </section>
 
 
